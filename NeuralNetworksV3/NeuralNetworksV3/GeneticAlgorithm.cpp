c76f205283276aaaab8b4d89a7dc123f46db3115
PeteSumners <32600547+PeteSumners@users.noreply.github.com>
2017-11-02 19:26:33 -0500

    The GeneticAlgorithm class is about 2/3 finished now.

--- NeuralNetworksV3/NeuralNetworksV3/GeneticAlgorithm.cpp
+++ NeuralNetworksV3/NeuralNetworksV3/GeneticAlgorithm.cpp
@@ -1,12 +1,242 @@
 #include "stdafx.h"
 #include "GeneticAlgorithm.h"
+#include <algorithm> // for std::sort
+#include <functional> // for std::greater
 
 
+#pragma region Fields
+#pragma region Default Parameters
+const float GeneticAlgorithm::defInitParamMin = -1.0f;
+
+const float GeneticAlgorithm::defInitParamMax = 1.0f;
+
+const float defCrossSwapProb = 0.6f;
+
+const float GeneticAlgorithm::defMutationProb = .3f;
+
+const float GeneticAlgorithm::defMutationAmount = 2.0f;
+
+const float GeneticAlgorithm::defMutationProp = 1.0f;
+#pragma endregion
+#pragma endregion
+
+#pragma region Methods
 GeneticAlgorithm::GeneticAlgorithm()
 {
 }
 
+void GeneticAlgorithm::Start() {
+	running = true;
 
-GeneticAlgorithm::~GeneticAlgorithm()
-{
+	InitializePopulation(currentPopulation);
+
+	// Load Genotypes into the current population if GenotypeLoadingMethod is not null.
+	if (GenotypeLoadingMethod != NULL)
+		GenotypeLoadingMethod(currentPopulation);
+
+	Evaluation(currentPopulation);
 }
+
+void GeneticAlgorithm::EvaluationFinished() {
+	// Calcuate a fitness from a recently-ended evaluation.
+	FitnessCalculationMethod(currentPopulation);
+
+	// Sort the population if sortPopulation is true.
+	if (sortPopulation)
+		std::sort(currentPopulation.begin, currentPopulation.end, std::greater<int>())();
+
+	/*
+	// Create a string that debugs information about the last evaluation.
+	std::string evalString = "Worst evaluation: " + currentPopulation[populationSize - 1].evaluation + "\n";
+
+	if (currentPopulation[0].evaluation == 0)
+		evalString += "Problem!\n" + currentPopulation[1].evaluation;
+	else
+		evalString += "Best evaluation: " + currentPopulation[0].evaluation + " (Genotype " + currentPopulation[0].genotypeIndex + ")";
+
+	Debug.Log(evalString);
+	*/
+
+	// Fire the FitnessCalculationFinished event if it's not null.
+	if (FitnessCalculationFinished != NULL)
+		FitnessCalculationFinished(currentPopulation);
+
+	// Check termination criteria, and terminate this GeneticAlgorithm if TerminationCriteria() returns true.
+	// (This is an example of shortcircuit evaluation!)
+	if (CheckTerminationCriteria != NULL && CheckTerminationCriteria(currentPopulation)) {
+		Terminate();
+		return;
+	}
+
+	// Now create a new population!
+	CreateNewPopulation();
+	Evaluation(currentPopulation);
+}
+
+void GeneticAlgorithm::CreateNewPopulation() {
+	// Apply the Selection method.
+	std::vector<Genotype> intermediatePopulation = SelectionOperator(currentPopulation);
+
+	// Apply the Recombination method.
+	std::vector<Genotype> newPopulation = RecombinationOperator(intermediatePopulation, populationSize);
+
+	// Apply the Mutation method.
+	MutationOperator(newPopulation);
+
+	// Set the current population to a newly generated one, and start evaluation again.
+	currentPopulation = newPopulation;
+	generationCount++;
+}
+
+void GeneticAlgorithm::Terminate() {
+	running = false;
+
+	// Save genotypes from currentPopulation if GenotypeSavingMethod is defined.
+	if (GenotypeSavingMethod != NULL)
+		GenotypeSavingMethod(currentPopulation);
+
+	// Call AlgorithmTerminated if it's not null.
+	if (AlgorithmTerminated != NULL) {
+		AlgorithmTerminated(*this);
+	}
+}
+
+#pragma region Static Methods
+#pragma region Default Operators
+	void GeneticAlgorithm::DefaultPopulationInitialization(std::vector<Genotype> population) {
+		// Set parameters to random values in a set range.
+		for (int i = 0; i < population.size(); i++) {
+			population[i].SetRandomParameters(defInitParamMin, defInitParamMax);
+		}
+	}
+
+	/// <summary>
+	/// Calculates the fitness of each genotype by the formula: fitness = evaluation / averageEvaluation.
+	/// </summary>
+	/// <param name="currentPopulation">The current population.</param>
+	void GeneticAlgorithm::DefaultFitnessCalculation(std::vector<Genotype> currentPopulation) {
+		// First calculate the average evaluation of the whole population.
+		int currentPopulationSize = 0;
+		float totalEvaluation = 0;
+		for (int i = 0; i < currentPopulation.size(); i++) {
+			totalEvaluation += currentPopulation[i].evaluation;
+			currentPopulationSize++;
+		}
+
+		float averageEvaluation = totalEvaluation / currentPopulationSize;
+
+		// Assign fitnesses with the formula: fitness = evaluation / averageEvaluation.
+		for (int i = 0; i < currentPopulation.size(); i++) {
+			currentPopulation[i].fitness = currentPopulation[i].evaluation / averageEvaluation;
+		}
+	}
+
+	/// <summary>
+	/// Only selects the best 3 genotypes of the current population, and copies them to an intermediate population.
+	/// </summary>
+	/// <returns>An intermediate population.</returns>
+	/// <param name="currentPopulation">The current population.</param>
+	std::vector<Genotype> GeneticAlgorithm::DefaultSelectionOperator(std::vector<Genotype> currentPopulation) {
+		// Check arguments
+		if (currentPopulation.size() < 3) {
+			throw new std::invalid_argument("The current population must have at least 3 genotypes for this selection operator to work!");
+		}
+
+		std::vector<Genotype> intermediatePopulation; intermediatePopulation.reserve(3);
+		intermediatePopulation[0] = currentPopulation[0];
+		intermediatePopulation[1] = currentPopulation[1];
+		intermediatePopulation[2] = currentPopulation[2];
+
+		return intermediatePopulation;
+	}
+
+	/// <summary>
+	/// Simply crosses the first genotype of the intermediate population with the second genotype of the intermediate 
+	/// population until the new population is of a given desired size.
+	/// </summary>
+	/// <returns>A new population.</returns>
+	/// <param name="intermediatePopulation">The intermediate population that was created from the selection process.</param>
+	/// <param name="newPopulationSize">The size of the new population.</param>
+	std::vector<Genotype> GeneticAlgorithm::DefaultRecombinationOperator(std::vector<Genotype> intermediatePopulation, int newPopulationSize) {
+		// Check arguments.
+		if (intermediatePopulation.Count < 2)
+			throw new ArgumentException("The intermediate population size must be at least 2 for this operator.");
+
+		if (newPopulationSize < 2)
+			throw new ArgumentException("The new population size must be at least 2 for this operator.");
+
+		std::vector<Genotype> newPopulation = new std::vector<Genotype>();
+
+		newPopulation.Add(intermediatePopulation[0]);
+		newPopulation.Add(intermediatePopulation[1]);
+
+		// Add new Genotypes to the population until there are newPopulationSize Genotypes in the population.
+		while (newPopulation.Count < newPopulationSize) {
+			Genotype offspring1, offspring2;
+			CompleteCrossover(intermediatePopulation[0], intermediatePopulation[1], defCrossSwapProb, out offspring1, out offspring2);
+
+			newPopulation.Add(offspring1);
+			// Don't add another Genotype to the population if there isn't room for another Genotype.
+			if (newPopulation.Count < newPopulationSize)
+				newPopulation.Add(offspring2);
+		}
+
+		return newPopulation;
+	}
+
+	/// <summary>
+	/// Simply mutates each genotype with the default mutation probability and amount.
+	/// </summary>
+	/// <param name="newPopulation">A new, mutated population.</param>
+	void GeneticAlgorithm::DefaultMutationOperator(std::vector<Genotype> newPopulation) {
+		for (int i = 0; i < newPopulation.size(); i++) (Genotype genotype in newPopulation) {
+			// Mutate approximately defMutationPerc of genotypes in the population.
+			if (UnityEngine.Random.Range(0f, 1f) < defMutationProp)
+				newPopulation[i] = MutateGenotype(newPopulation[i], defMutationProb, defMutationAmount);
+		}
+	}
+#pragma endregion
+
+#pragma region Recombination Operators
+	/// <summary>
+	/// Gives offspring genotypes parameters from their respective parent genotypes, swapping the parents from which offspring get parameters based on swapChance.
+	/// </summary>
+	void GeneticAlgorithm::CompleteCrossover(Genotype parent1, Genotype parent2, float swapChance, out Genotype offspring1, out Genotype offspring2) {
+		// Initialize new parameter vectors.
+		int parameterCount = parent1.GetParameterCount();
+
+		// off1Parameters is an array of floats representing offspring1's parameters.
+		std::vector<float> off1Parameters; off1Parameters.reserve(parameterCount);
+		std::vector<float> off2Parameters; off2Parameters.reserve(parameterCount);
+
+		// Iterate over all of the parents' parameters, giving parameters from parents to their respective offspring.
+		for (int i = 0; i < parameterCount; i++) {
+			if (MathHelper.Random.Range(0f, 1f) < swapChance) {
+				// Swap parent-to-offspring parameters.
+				off1Parameters[i] = parent2[i];
+				off2Parameters[i] = parent1[i];
+			}
+			else {
+				// Don't swap parent-to-offspring parameters.
+				off1Parameters[i] = parent1[i];
+				off2Parameters[i] = parent2[i];
+			}
+		}
+
+		offspring1 = new Genotype(off1Parameters);
+		offspring2 = new Genotype(off2Parameters);
+	}
+#pragma endregion
+
+#pragma region Mutation Operators
+	static Genotype MutateGenotype(Genotype genotype, float mutationProb, float mutationAmount) {
+		for (int i = 0; i < genotype.GetParameterCount(); i++) {
+			if (MathHelper.RandomRange(0f, 1f) < mutationProb) {
+				// Mutate by a random amount in the range [-mutationAmount, mutationAmount].
+				genotype.SetParameter(i, (genotype.GetParameter(i) + MathHelper.RandomRange(-1f, 1f) * mutationAmount));
+			}
+		}
+	}
+#pragma endregion
+#pragma endregion
+#pragma endregion
--- NeuralNetworksV3/NeuralNetworksV3/GeneticAlgorithm.h
+++ NeuralNetworksV3/NeuralNetworksV3/GeneticAlgorithm.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Genotype.h"
 #include <vector>
+
+//TODO: Add the MathHelper class. Add a RandomRange (min, max) function to the MathHelper class.
 class GeneticAlgorithm
 {
 
@@ -16,32 +18,32 @@ public:
 	/// <summary>
 	/// The default min value of initial population parameters.
 	/// </summary>
-	const float defInitParamMin = -1.0f;
+	static const float defInitParamMin;
 
 	/// <summary>
 	/// The default max value of initial population parameters.
 	/// </summary>
-	const float defInitParamMax = 1.0f;
+	static const float defInitParamMax;
 
 	/// <summary>
 	/// The default probability of a parameter being swapped during crossover.
 	/// </summary>
-	const float defCrossSwapProb = 0.6f;
+	static const float defCrossSwapProb = 0.6f;
 
 	/// <summary>
 	/// The default probability of a parameter being mutated.
 	/// </summary>
-	const float defMutationProb = .3f;
+	static const float defMutationProb;
 
 	/// <summary>
 	/// The default amount by which parameters may be mutated.
 	/// </summary>
-	const float defMutationAmount = 2.0f;
+	static const float defMutationAmount;
 
 	/// <summary>
 	/// The default proportion of genotypes in a new population that are mutated.
 	/// </summary>
-	const float defMutationPerc = 1.0f;
+	static const float defMutationProp;
 	#pragma endregion
 
 // TODO: Wire the correct methods to the below function pointers.
@@ -72,7 +74,7 @@ public:
 	/// <summary>
 	/// Function pointer used to calculate the fitness value of each genotype of the current population.
 	/// </summary>
-	void (*FitnessCalculation)(std::vector<Genotype> currentPopulation) = NULL;
+	void (*FitnessCalculationMethod)(std::vector<Genotype> currentPopulation) = NULL;
 
 	/// <summary>
 	/// Function pointer used to select genotypes of the current population and create an intermediate population.
@@ -99,54 +101,7 @@ public:
 	bool (*CheckTerminationCriteria)(std::vector<Genotype> currentPopulation) = NULL;
 	#pragma endregion
 
-	/*#pragma region Operator Methods
-	/// <summary>
-	/// Method used to initialize an initial population.
-	/// </summary>
-	InitializationOperator InitializePopulation = DefaultPopulationInitialization;
-
-	/// <summary>
-	/// Method used to load genotypes into an initial population.
-	/// </summary>
-	public GenotypeLoadingOperator GenotypeLoadingMethod;
-
-	/// <summary>
-	/// Method used to save genotypes found in a final population.
-	/// </summary>
-	public GenotypeSavingOperator GenotypeSavingMethod;
-
-	/// <summary>
-	/// Method used to evaluate (or start the evaluation process of) the current population.
-	/// </summary>
-	public EvaluationOperator Evaluation;
-
-	/// <summary>
-	/// Method used to calculate the fitness value of each genotype in the current population.
-	/// </summary>
-	public FitnessCalculation FitnessCalculationMethod = DefaultFitnessCalculation;
-
-	/// <summary>
-	/// Method used to select genotypes of the current population and create an intermediate population.
-	/// </summary>
-	public SelectionOperator Selection = DefaultSelectionOperator;
-
-	/// <summary>
-	/// Method used to recombine an intermediate population to generate a new population.
-	/// </summary>
-	public RecombinationOperator Recombination = DefaultRecombinationOperator;
-
-	/// <summary>
-	/// Method used to mutate a new population.
-	/// </summary>
-	public MutationOperator Mutation = DefaultMutationOperator;
-
-	/// <summary>
-	/// Method used to check whether any termination criterion has been met.
-	/// </summary>
-	public CheckTerminationCriteria TerminationCriteria = null;
-	#pragma endregion
-	*/
-
+#pragma region General
 private:
 	/// <summary>
 	/// The current population.
@@ -177,14 +132,15 @@ public:
 	/// <summary>
 	/// Event for when this algorithm is eventually terminated.
 	/// </summary>
-	void (*AlgorithmTerminated)(GeneticAlgorithm ga);
+	void (*AlgorithmTerminated)(GeneticAlgorithm ga) = NULL;
 
 	/// <summary>
 	/// Event for when this algorithm has finished a fitness calculation. The given parameter
 	/// is the current population sorted by fitness, if sorting is enabled (see <see cref="SortPopulation"/>).
 	/// </summary>
-	void (*FitnessCalculationFinished)(std::vector<Genotype> genotypes);
-	#pragma endregion
+	void (*FitnessCalculationFinished)(std::vector<Genotype> genotypes) = NULL;
+#pragma endregion
+#pragma endregion
 
 #pragma region Constructors
 	public:
@@ -212,143 +168,47 @@ public:
 #pragma endregion
 
 #pragma region Methods
-	public:
-		void Start() {
-		running = true;
-
-		InitializePopulation(currentPopulation);
-
-		// Load Genotypes into the current population if GenotypeLoadingMethod is not null.
-		if (GenotypeLoadingMethod != NULL)
-			GenotypeLoadingMethod(currentPopulation);
-
-		Evaluation(currentPopulation);
-	}
+public:
+	void Start();
 
 	/// <summary>
 	/// Evaluates a population after it has run its course.
 	/// </summary>
-	public void EvaluationFinished() {
-		// Calcuate a fitness from a recently-ended evaluation.
-		FitnessCalculationMethod(currentPopulation);
+	void EvaluationFinished();
 
-		// Sort the population if sortPopulation is true.
-		if (sortPopulation)
-			currentPopulation.Sort();
-
-		// Create a string that debugs information about the last evaluation.
-		string evalString = "Worst evaluation: " + currentPopulation[populationSize - 1].evaluation + "\n";
-
-		if (currentPopulation[0].evaluation == 0)
-			evalString += "Problem!\n" + currentPopulation[1].evaluation;
-		else
-			evalString += "Best evaluation: " + currentPopulation[0].evaluation + " (Genotype " + currentPopulation[0].genotypeIndex + ")";
-
-		Debug.Log(evalString);
-
-		// Fire the FitnessCalculationFinished event if it's not null.
-		if (FitnessCalculationFinished != null)
-			FitnessCalculationFinished(currentPopulation);
-
-		// Check termination criteria, and terminate this GeneticAlgorithm if TerminationCriteria() returns true.
-		// (This is an example of shortcircuit evaluation!)
-		if (TerminationCriteria != null && TerminationCriteria(currentPopulation)) {
-			Terminate();
-			return;
-		}
-
-		// Now create a new population!
-		CreateNewPopulation();
-		Evaluation(currentPopulation);
-	}
-
+private:
 	/// <summary>
 	/// Creates a new population of genotypes, and increments generationCount by 1.
 	/// (Assumes that currentPopulation has been sorted.)
 	/// </summary>
-	private void CreateNewPopulation() {
-		// Apply the Selection method.
-		std::vector<Genotype> intermediatePopulation = Selection(currentPopulation);
-
-		// Apply the Recombination method.
-		std::vector<Genotype> newPopulation = Recombination(intermediatePopulation, populationSize);
-
-		// Apply the Mutation method.
-		Mutation(newPopulation);
-
-		// Set the current population to a newly generated one, and start evaluation again.
-		currentPopulation = newPopulation;
-		generationCount++;
-	}
+	void CreateNewPopulation();
 
 	/// <summary>
 	/// Terminates this GeneticAlgorithm.
 	/// </summary>
-	private void Terminate() {
-		running = false;
-
-		// Save genotypes from currentPopulation if GenotypeSavingMethod is defined.
-		if (GenotypeSavingMethod != null)
-			GenotypeSavingMethod(currentPopulation);
-
-		// Call AlgorithmTerminated if it's not null.
-		if (AlgorithmTerminated != null) {
-			AlgorithmTerminated(this);
-		}
-	}
+	void Terminate();
 
 #pragma region Static Methods
 #pragma region Default Operators
-		/// <summary>
-		/// Initializes the population by setting each parameter in each genotype to a random value in a default range.
-		/// </summary>
-		/// <param name="population">The population to be initialized.</param>
-		public static void DefaultPopulationInitialization(std::vector<Genotype> population) {
-		// Set parameters to random values in a set range.
-		foreach(Genotype genotype in population) {
-			genotype.SetRandomParameters(defInitParamMin, defInitParamMax);
-		}
-	}
+public:
+	/// <summary>
+	/// Initializes the population by setting each parameter in each genotype to a random value in a default range.
+	/// </summary>
+	/// <param name="population">The population to be initialized.</param>
+	static void DefaultPopulationInitialization(std::vector<Genotype> population);
 
 	/// <summary>
 	/// Calculates the fitness of each genotype by the formula: fitness = evaluation / averageEvaluation.
 	/// </summary>
 	/// <param name="currentPopulation">The current population.</param>
-	public static void DefaultFitnessCalculation(std::vector<Genotype> currentPopulation) {
-		// First calculate the average evaluation of the whole population.
-		int currentPopulationSize = 0;
-		float totalEvaluation = 0;
-		foreach(Genotype genotype in currentPopulation) {
-			totalEvaluation += genotype.evaluation;
-			currentPopulationSize++;
-		}
-
-		float averageEvaluation = totalEvaluation / currentPopulationSize;
-
-		// Assign fitnesses with the formula: fitness = evaluation / averageEvaluation.
-		foreach(Genotype genotype in currentPopulation) {
-			genotype.fitness = genotype.evaluation / averageEvaluation;
-		}
-	}
+	static void DefaultFitnessCalculation(std::vector<Genotype> currentPopulation);
 
 	/// <summary>
 	/// Only selects the best 3 genotypes of the current population, and copies them to an intermediate population.
 	/// </summary>
 	/// <returns>An intermediate population.</returns>
 	/// <param name="currentPopulation">The current population.</param>
-	public static std::vector<Genotype> DefaultSelectionOperator(std::vector<Genotype> currentPopulation) {
-		// Check arguments
-		if (currentPopulation.Count < 3) {
-			throw new ArgumentException("The current population must have at least 3 genotypes for this selection operator to work!");
-		}
-
-		std::vector<Genotype> intermediatePopulation = new std::vector<Genotype>();
-		intermediatePopulation.Add(currentPopulation[0]);
-		intermediatePopulation.Add(currentPopulation[1]);
-		intermediatePopulation.Add(currentPopulation[2]);
-
-		return intermediatePopulation;
-	}
+	static std::vector<Genotype> DefaultSelectionOperator(std::vector<Genotype> currentPopulation);
 
 	/// <summary>
 	/// Simply crosses the first genotype of the intermediate population with the second genotype of the intermediate 
@@ -357,91 +217,32 @@ public:
 	/// <returns>A new population.</returns>
 	/// <param name="intermediatePopulation">The intermediate population that was created from the selection process.</param>
 	/// <param name="newPopulationSize">The size of the new population.</param>
-	public static std::vector<Genotype> DefaultRecombinationOperator(std::vector<Genotype> intermediatePopulation, int newPopulationSize) {
-		// Check arguments.
-		if (intermediatePopulation.Count < 2)
-			throw new ArgumentException("The intermediate population size must be at least 2 for this operator.");
-
-		if (newPopulationSize < 2)
-			throw new ArgumentException("The new population size must be at least 2 for this operator.");
-
-		std::vector<Genotype> newPopulation = new std::vector<Genotype>();
-
-		newPopulation.Add(intermediatePopulation[0]);
-		newPopulation.Add(intermediatePopulation[1]);
-
-		// Add new Genotypes to the population until there are newPopulationSize Genotypes in the population.
-		while (newPopulation.Count < newPopulationSize) {
-			Genotype offspring1, offspring2;
-			CompleteCrossover(intermediatePopulation[0], intermediatePopulation[1], defCrossSwapProb, out offspring1, out offspring2);
-
-			newPopulation.Add(offspring1);
-			// Don't add another Genotype to the population if there isn't room for another Genotype.
-			if (newPopulation.Count < newPopulationSize)
-				newPopulation.Add(offspring2);
-		}
-
-		return newPopulation;
-	}
+	static std::vector<Genotype> DefaultRecombinationOperator(std::vector<Genotype> intermediatePopulation, int newPopulationSize);
 
 	/// <summary>
 	/// Simply mutates each genotype with the default mutation probability and amount.
 	/// </summary>
 	/// <param name="newPopulation">A new, mutated population.</param>
-	public static void DefaultMutationOperator(std::vector<Genotype> newPopulation) {
-		foreach(Genotype genotype in newPopulation) {
-			// Mutate approximately defMutationPerc of genotypes in the population.
-			if (UnityEngine.Random.Range(0f, 1f) < defMutationPerc)
-				MutateGenotype(genotype, defMutationProb, defMutationAmount);
-		}
-	}
+	static void DefaultMutationOperator(std::vector<Genotype> newPopulation);
 #pragma endregion
 
 #pragma region Recombination Operators
+public:
 	/// <summary>
 	/// Gives offspring genotypes parameters from their respective parent genotypes, swapping the parents from which offspring get parameters based on swapChance.
 	/// </summary>
-	public static void CompleteCrossover(Genotype parent1, Genotype parent2, float swapChance, out Genotype offspring1, out Genotype offspring2) {
-		// Initialize new parameter vectors.
-		int parameterCount = parent1.parameterCount;
-
-		// off1Parameters is an array of floats representing offspring1's parameters.
-		float[] off1Parameters = new float[parameterCount], off2Parameters = new float[parameterCount];
-
-		// Iterate over all of the parents' parameters, giving parameters from parents to their respective offspring.
-		for (int i = 0; i < parameterCount; i++) {
-			if (UnityEngine.Random.Range(0f, 1f) < swapChance) {
-				// Swap parent-to-offspring parameters.
-				off1Parameters[i] = parent2[i];
-				off2Parameters[i] = parent1[i];
-			}
-			else {
-				// Don't swap parent-to-offspring parameters.
-				off1Parameters[i] = parent1[i];
-				off2Parameters[i] = parent2[i];
-			}
-		}
-
-		offspring1 = new Genotype(off1Parameters);
-		offspring2 = new Genotype(off2Parameters);
-	}
+	static void CompleteCrossover(Genotype parent1, Genotype parent2, float swapChance, out Genotype offspring1, out Genotype offspring2);
 #pragma endregion
 
 #pragma region Mutation Operators
+public:
 	/// <summary>
 	/// Mutates a given genotype by adding a random value in the range [-mutationAmount, mutationAmount] to each parameter with a probability of mutationProb.
 	/// </summary>
 	/// <param name="genotype">The genotype to be mutated.</param>
 	/// <param name="mutationProb">The probability of a parameter being mutated.</param>
 	/// <param name="mutationAmount">A parameter may be mutated by an amount in the range [-mutationAmount, mutationAmount].</param>
-	public static void MutateGenotype(Genotype genotype, float mutationProb, float mutationAmount) {
-		for (int i = 0; i < genotype.parameterCount; i++) {
-			if (UnityEngine.Random.Range(0f, 1f) < mutationProb) {
-				// Mutate by a random amount in the range [-mutationAmount, mutationAmount].
-				genotype[i] += UnityEngine.Random.Range(-1f, 1f) * mutationAmount;
-			}
-		}
-	}
+	static Genotype MutateGenotype(Genotype genotype, float mutationProb, float mutationAmount);
 #pragma endregion
 #pragma endregion
 #pragma endregion
