b76bb9141c083cafa5b3cbafc067e6f6883a40c4
mario <airvex@utexas.edu>
2017-11-10 15:48:05 -0600

    Made slight modifications to roomasim.world code; improved readability and added <cylinder> line which has the potential to spawn roombas in a circle. Need to learn how to spawn the roombas right on the circle though.

--- roomba_host/roombasim.cc
+++ roomba_host/roombasim.cc
@@ -23,8 +23,8 @@ namespace gazebo
     public: void OnUpdate(const common::UpdateInfo & /*_info*/)
     {
       // This will be the roomba behavior
-	this->model->GetJoint("left_wheel")->SetParam("fmax",0,100.0);
-	this->model->GetJoint("left_wheel")->SetParam("vel",0,20.0);
+	this->model->GetJoint("left_wheel")->SetParam("fmax",0,200.0);
+	this->model->GetJoint("left_wheel")->SetParam("vel",0,30.0);
 	
     }
 
