e30d9f63da9a8e8d468ab573258556cd55e6bf30
PeteSumners <pete.sumners@utexas.edu>
2018-01-27 12:50:38 -0600

    Added Mutation and DeepCopy functions, so NNs can change now. Still need a helper method for mutating weights and a true NN constructor.

--- LeanNNs/NeuralNetwork.cpp
+++ LeanNNs/NeuralNetwork.cpp
@@ -4,7 +4,62 @@
 
 #include "NeuralNetwork.h"
 
-int* NeuralNetwork::FeedForward(float *input, int32_t inputLength) { // Cool! float* is the same as float[]!
+// TODO: Implement activation and weight mutation functions.
+
+NeuralNetwork NeuralNetwork::DeepCopy(NeuralNetwork nnToCopy) {
+    int layerCount = nnToCopy.layerCount;
+    int topology[layerCount];
+
+    for (int32_t i = 0; i < layerCount; i++) {
+        topology[i] = nnToCopy.topology[i];
+    }
+
+    // Just deep copy weights: neurons and output are essentially temp variables:
+    int32_t weightCount = nnToCopy.weightCount;
+    float weights[weightCount];
+
+    int32_t prevLayerNeurons = 0; // Total neuron count of all previous layers.
+    int32_t currentLayerNeurons = 0; // Neuron count of current layer.
+    for (int32_t i = 0; i < layerCount - 1; i++) { // Current layer
+        currentLayerNeurons = topology[i];
+
+        for (int32_t j = 0; j < currentLayerNeurons; j++) { // Current neuron, current layer.
+            for (int32_t k = 0; k < topology[i + 1]; k++) { // Current neuron, next layer.
+                weights[prevLayerNeurons + currentLayerNeurons + k] =
+                        nnToCopy.weights[prevLayerNeurons + currentLayerNeurons + k];
+            }
+
+        }
+
+        prevLayerNeurons += currentLayerNeurons;
+    }
+}
+
+// Note: to get output length, just get NeuralNetwork's topology[layerCount - 1]!
+int32_t* NeuralNetwork::FeedForward(float* input, int32_t inputLength) { // Cool! float* is the same as float[]!
+    int32_t prevLayerNeurons = 0; // Total neuron count of all previous layers.
+    int32_t currentLayerNeurons = 0; // Neuron count of current layer.
+    for (int32_t i = 0; i < layerCount - 1; i++) { // Current layer
+        currentLayerNeurons = topology[i];
+
+        for (int32_t j = 0; j < currentLayerNeurons; j++) { // Current neuron, current layer.
+            for (int32_t k = 0; k < topology[i + 1]; k++) { // Current neuron, next layer.
+                neurons[prevLayerNeurons + currentLayerNeurons + k] += ActivationFunction(neurons[prevLayerNeurons + j]);
+            }
+
+        }
+
+        prevLayerNeurons += currentLayerNeurons;
+    }
+
+    prevLayerNeurons -= currentLayerNeurons; // Get back to the START of the last layer.
+    for (int32_t i = prevLayerNeurons, j = 0; j < topology[layerCount - 1]; i++, j++) {
+        output[j] = neurons[i];
+    }
+    return (int32_t*) output;
+}
+
+void NeuralNetwork::Mutate() {
     int32_t prevLayerNeurons = 0; // Total neuron count of all previous layers.
     int32_t currentLayerNeurons = 0; // Neuron count of current layer.
     for (int32_t i = 0; i < layerCount - 1; i++) { // Current layer
@@ -13,7 +68,8 @@ int* NeuralNetwork::FeedForward(float *input, int32_t inputLength) { // Cool! fl
         for (int32_t j = 0; j < currentLayerNeurons; j++) { // Current neuron, current layer.
 
             for (int32_t k = 0; k < topology[i + 1]; k++) { // Current neuron, next layer.
-                neurons[prevLayerNeurons + currentLayerNeurons + k] += ActivationFunction(neurons[prevLayerNeurons + j]);
+                // Mutate current weight!
+                weights[prevLayerNeurons + currentLayerNeurons + k] = MutateWeight (weights[prevLayerNeurons + currentLayerNeurons + k]);
             }
 
         }
--- LeanNNs/NeuralNetwork.h
+++ LeanNNs/NeuralNetwork.h
@@ -14,22 +14,46 @@ class NeuralNetwork {
  * Mutation function
  * Copy function
  * Random creation function
+ * File IO (saving and loading NNs)
 */
+
+// TODO: Random creation function, file IO.
 
     // 1-dimensional.
     int32_t topology[];
-    int32_t layerCount;
+    int32_t layerCount; // How many layers this NN has.
 
     // 2-dimensional
     float neurons[];
 
     // 3-dimensional
     float weights[];
+    int32_t weightCount;
+
+    // Current output.
+    float output[]; // TODO: Init. output: float output[topology[layerCount - 1]];
+    int32_t outputCount; // TODO: init. output count and other counts!
+
+    const float weightMutChance = .5; // Chance of a given weight being mutated.
+    const float minWeightMutProp = .5; // Min possible new weight value, prop. of weight's curr. val.
+    const float maxWeightMutProp = 2; // Max possible new weight value as proportion of weight's current value
+
+    // Input: Old NN to copy.
+    // Output: New NN w/ copied fields.
+    NeuralNetwork DeepCopy (NeuralNetwork nnToCopy);
 
     // Input: float array w/ its length
     // Returns a pointer to a float array representing this NN's output.
-    int* FeedForward (float input[], int inputLength);
-};
+    int32_t* FeedForward(float input[], int inputLength);
 
+    // Mutates this NN.
+    void Mutate();
+
+    // Mutates the given weight if it should be mutated, returns new weight.
+    static float MutateWeight(float input);
+
+    // Implements a SoftSign activation function.
+    static float ActivationFunction(float input);
+};
 
 #endif //LEANNNS_NEURALNETWORK_H
