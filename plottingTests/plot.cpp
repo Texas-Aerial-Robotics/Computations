5462c0836992b070bb69d1ac7e64aa1a7dfe4d6e
Umer Salman <umer936@gmail.com>
2018-06-07 22:02:23 -0500

    Made plotting I N T E R A C T I V E and added comments and added x,y limits

--- plottingTests/plot.cpp
+++ plottingTests/plot.cpp
@@ -7,18 +7,24 @@ namespace plt = matplotlibcpp;
 int main() {
 	x.push_back(1); // Add value 1 to first position in x array of values
 	y.push_back(3); // Add value 3 to first position in x array of values
-	plt::plot(x, y, "r-"); // Plot "x, y" as "red and dashed"
-	plt::show(); // Show in the window
+	plt::ion(); // Make interactive
+	plt::ylim(-10,10); // Set y limits
+	plt::xlim(-10,10); // Set x limits
+	plt::plot(x, y, "ro"); // Plot "x, y" as "red and dashed"
+	plt::pause(1); // Just needs to be larger than 0.0001
 	x.push_back(2);
 	y.push_back(4);
-	plt::plot(x, y, "r-");
-	plt::show();
+	plt::plot(x, y, "ro");
+	plt::pause(1);
+	plt::draw();
 	x.push_back(3);
 	y.push_back(2);
-	plt::plot(x, y, "r-");
-	plt::show();
+	plt::plot(x, y, "ro");
+	plt::pause(1);
+	plt::draw();
 	x.push_back(2);
 	y.push_back(2.5);
-	plt::plot(x, y, "r-");
-	plt::show();
+	plt::plot(x, y, "ro");
+	plt::pause(1);
+	plt::draw();
 }
